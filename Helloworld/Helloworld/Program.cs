267c72c30085414705140f541bac3615aae47c18
DinabMorgan <morgan.dinab@gmail.com>
2022-05-18 14:39:18 -0400

    changes

---
+++ Helloworld/Helloworld/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hi Friends!");
+Console.WriteLine();
+// please ignore what we are about to write, dont read this as a command, 
+
+// variable explainations
+//  What is a variable: these are named objects and values that can change
+//  variable Type VariableName;
+
+string instructor; // this variable is NULL - has no value
+// variable assignments - this is giving a variable a value
+// variable name = value
+
+instructor = "Davis"; // sometimes called setting a value - assigning it a value
+Console.WriteLine(instructor);
+string instructor1;
+instructor1 = "Gavin";
+Console.WriteLine (instructor1);
+
+
+string instructor3 = "Dina";
+Console.WriteLine("Computer" + instructor3);
+Console.WriteLine ("What is your name?");
+string input = Console.ReadLine();
+
+
+Console.WriteLine("nice to meet you, "+ input);
+
+int num = 5 + 5;
+num = num + 1;
+Console.WriteLine(num == 10);
+float num1 = 5 / 3f;
+Console.WriteLine(num1);
+Console.WriteLine("would you give a wolf a banana?");
+ Console.WriteLine("Y/N");
+   String banana =  Console.ReadLine();
+Console.WriteLine("I cant believe you chose  " + banana);
+
+
